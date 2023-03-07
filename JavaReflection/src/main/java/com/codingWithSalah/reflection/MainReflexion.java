9a149d559da09223c3e88f89a323a0c1f5fe55eb
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-03-07 10:58:31 +0100

    Java reflexion

---
+++ JavaReflection/src/main/java/com/codingWithSalah/reflection/MainReflexion.java
@@ -0,0 +1,31 @@
+package com.codingWithSalah.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainReflexion {
+    public static void main(String[] args) throws Exception {
+        Cat myCat=new Cat("rex",6);
+        Field[] catFields =myCat.getClass().getDeclaredFields();
+        for(Field field: catFields){
+            if(field.getName().equals("name")){
+                field.setAccessible(true);
+                field.set(myCat,"rex fex");
+            }
+        }
+        System.out.println(myCat.getName());
+
+        Method[] catMethod=myCat.getClass().getDeclaredMethods();
+        for (Method method: catMethod){
+            if(method.getName().equals("heyThisIsPrivate") ){
+                method.setAccessible(true);
+                method.invoke(myCat);
+            } else if (method.getName().equals("thisIsAPrivateStaticMethod")) {
+                method.setAccessible(true);
+                method.invoke(null);// because there is No Object just the class
+            }
+        }
+
+    }
+
+}
